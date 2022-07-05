582c221be8824184af929f5d6c306c6e1c3078a9
Shubham Anand Gupta <shubhamanandgupta@outlook.com>
2022-07-05 22:12:03 +0530

    new bubble

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,12 +1,12 @@
 class BubbleSort {
     public static void main(String[] args) {
         BubbleSort a = new BubbleSort();
-        int[] A = { 32, 124, 52, 89, 686, 678 };
-        int[] B = { 312, 11, 45, 43 };
+        int[] A = { 7885, 32, 124, 52, 89, 686, 678, 642, 6543 };
+        int[] B = { 312, 11, 45, 67, -4, 43 };
         A = a.sort(A);
-        B = a.sort(B);
         System.out.println("Sorted A :");
         a.display_arr(A);
+        B = a.sort(B);
         System.out.println("Sorted B :");
         a.display_arr(B);
         System.out.println("Merged Array :");
@@ -14,12 +14,15 @@ class BubbleSort {
     }
 
     int[] sort(int[] arr) {
-        for (int x = 0; x < arr.length - 1; x++) {
-            for (int y = 0; y < arr.length - x - 1; y++) {
+        boolean flag = true;
+        while (flag) {
+            flag = false;
+            for (int y = 0; y < arr.length - 1; y++) {
                 if (arr[y] > arr[y + 1]) {
                     int tmp = arr[y];
                     arr[y] = arr[y + 1];
                     arr[y + 1] = tmp;
+                    flag = true;
                 }
             }
         }
